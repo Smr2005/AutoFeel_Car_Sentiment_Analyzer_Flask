db952688bcf202fe34b940c3a0141b6d18cbc83a
Smr2005 <shaiksameershubhan@gmail.com>
2025-07-03 11:48:27 +0530

    app.py

--- app.py
+++ app.py
@@ -193,6 +193,6 @@ def manufacturer_insights():
     )
 
 # Run App
-if _name_ == '_main_':
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # Render sets PORT environment variable
     app.run(debug=True, host='0.0.0.0', port=port)
